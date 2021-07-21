5465b32daa71231505de59c30217c43a2462a4ff
Aleksandr Serdyuk <a.galko83@gmail.com>
2021-07-21 21:14:09 +0300

    fixed bag's

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 // @ts-check
 
+// import botstrap from 'bootstrap';
 import init from './init.js';
-import './style.scss';
+// import './style.scss';
+import '../node_modules/bootstrap/dist/css/bootstrap.css';
 
 init();
