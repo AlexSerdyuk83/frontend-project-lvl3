a97a528e2f1aa028ccc7695f17e4b0cde63376bd
Aleksandr Serdyuk <a.galko83@gmail.com>
2021-07-22 23:03:31 +0300

    fixed bag's for development

--- src/init.js
+++
@@ -1,8 +0,0 @@
-// @ts-check
-
-// export default () => {
-//   // const element = document.getElementById('point');
-//   // element.classList.add('hello');
-//   // element.textContent = 'Hello, World';
-//   // return element;
-// };
